e1f2fc4af705ca773379979f7d84538e70392163
ITRS-333 <itrs-333@ITRS-333s-MacBook-Air.local>
2020-06-01 11:11:31 +0530

    connecting mongodb with the app

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+//MongoDB configuration 
+const db = require('./setup/myurl').mongoURL 
+
+//Attemt to conncet db
+mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true  },)
+.then(()=>{
+    console.log('mongo db connected successfully')
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+//route test
 app.get('/',(req,res)=>{
     res.send('hello big stsck')
-})
+});
 
 const port = process.env.PORT || 4000;
 
 app.listen(port,(req,res)=>{
     console.log('your server is running');
-    
 })
