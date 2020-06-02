7f03c4f20276cdb91edb885f69c61d877390a468
ITRS-333 <itrs-333@ITRS-333s-MacBook-Air.local>
2020-06-02 18:18:17 +0530

    adding basic profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Person = require('../../models/person');
+const Profile = require('../../models/Profile')
 
 //@type  -  GET
 //@route  -  /api/profile
-//@desc  -  just for testing
-//@access  -  PUBLIC
-router.get('/',(req,res)=>{
-    res.json({test:'profile is success'})
-});
+//@desc  -  route for personal user profile
+//@access  -  PRIVATE
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.findOne({user : req.user.id})
+    .then((profile)=>{
+        if(!profile){
+            res.status(404).json({profileError : "No profile found"})
+        }
+        res.json(profile)
+    })
+    .catch((err)=>{
+        console.log("got some error in profile" + err);
+        
+    })
+})
 
 module.exports = router;
