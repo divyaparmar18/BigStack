14c4a0718e2bfb311566f2be41ffbc0b0c9b2ef1
ITRS-333 <itrs-333@ITRS-333s-MacBook-Air.local>
2020-06-02 10:23:18 +0530

    adding login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,6 @@ const key = require('../../setup/myurl')
 //@route  -  /api/auth
 //@desc  -  just for testing
 //@access  -  PUBLIC
-
 router.get('/',(req,res)=>{
     res.json({test:'auth is success'})
 });
@@ -22,44 +21,81 @@ const Person = require('../../models/person');
 //@route  -  /api/auth/register
 //@desc  -  route for registration of users
 //@access  -  PUBLIC
-
 router.post('/register',(req,res)=>{
-    Person.findOne({email : req.body.email})
-    .then((person)=>{
-        if(person){
-            res.status(400).json({emailError : 'This email is already regitered on the site'})
-        }
-        else{
-            const newPerson = new Person({
-                name : req.body.name,
-                email : req.body.email,
-                password : req.body.password
-            });
-            //Encrypt password using bcrypt
-            bcrypt.genSalt(5,(err,salt)=>{
-                bcrypt.hash(newPerson.password,salt,(err,hash)=>{
-                    if(err) throw err;
-                    newPerson.password = hash;
-                    newPerson
-                    .save()
-                    .then((person)=>{
-                        res.json(person)
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name
+    if(email !== undefined && password !== undefined && name !== undefined){
+        Person.findOne({email})
+        .then((person)=>{
+            if(person){
+                res.status(400).json({emailError : 'This email is already regitered on the site'})
+            }
+            else{
+                const newPerson = new Person({
+                    name : name,
+                    email : email,
+                    password : password
+                });
+                //Encrypt password using bcrypt
+                bcrypt.genSalt(5,(err,salt)=>{
+                    bcrypt.hash(newPerson.password,salt,(err,hash)=>{
+                        if(err) console.log(err);
+                        newPerson.password = hash;
+                        newPerson
+                        .save()
+                        .then((person)=>{
+                            res.json(person)
+                        })
+                        .catch((err)=>{
+                            console.log(err);
+                        });
                     })
-                    .catch((err)=>{
-                        console.log(err);
-                        
-                    });
                 })
-            })
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+    }
+    else{
+        res.status(400).json({Error : "Email, name and passwords are required"})
+    }
+})
 
+//@type  -  POST
+//@route  -  /api/auth/login
+//@desc  -  route for login of users
+//@access  -  PUBLIC
+router.post('/login',(req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    if(email !== undefined && password !== undefined ){
+    Person.findOne({email})
+    .then((person)=>{
+        if(!person){
+            return res.status(400).json({emailError : "user not found with this email you can register"})
         }
-
-
+         bcrypt.compare(password,person.password)
+            .then((isCorrect)=>{
+             if(isCorrect){
+                 res.json({sucess : 'login successfully'})
+             }
+             else{
+                 res.status(400).json({passwprdError:'passwprd is not correct'})
+             }
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
     })
     .catch((err)=>{
         console.log(err);
-        
     })
-})
+}
+else{
+    res.status(400).json({Error : "email and password is required"})
+    }
+});
 
 module.exports = router;
